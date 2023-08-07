1334c8cec18e80da9a8975ca96a16cca6d24dc25
KimYeongim0 <ygim4029@gmail.com>
2023-08-07 13:58:35 +0900

    12

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js Apps"/>
+  <HelloWorld msg="Welcome to Your Vue.js Aps"/>
 </template>
 
 <script>
